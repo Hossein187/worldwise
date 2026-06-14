88b4696de3a543433febe4bf774736cee434bc86
Hossein187 <146576567+Hossein187@users.noreply.github.com>
2026-06-14 18:49:32 +0330

    update

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,10 +1,15 @@
 import Spinner from "./Spinner";
+import Message from "./Message";
+import CityItem from "./CityItem";
 
 import styles from "./CityList.module.css";
-import CityItem from "./CityItem";
 
 function CityList({ isLoading, cities }) {
   if (isLoading) return <Spinner />;
+  if (!cities.length)
+    return (
+      <Message message={"now We're on updating data, Please ComeBack Later."} />
+    );
   return (
     <ul className={styles.cityList}>
       {cities.map((city) => (
